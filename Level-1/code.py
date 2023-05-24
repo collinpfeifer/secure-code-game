8e5e8c115e8166f0e5d932cbcf9f250b0fc9fe3e
Collin Pfeifer <collinpfeifer@Icloud.com>
2023-05-24 01:16:45 +0000

    solved level 1

--- Level-1/code.py
+++ Level-1/code.py
@@ -1,4 +1,4 @@
-'''
+"""
 ////////////////////////////////////////////////////////////
 ///                                                      ///
 ///   0. tests.py is passing but the code is vulnerable  /// 
@@ -9,25 +9,28 @@
 ///   5. Compare your solution with solution.py          ///
 ///                                                      ///
 ////////////////////////////////////////////////////////////
-'''
+"""
 
 from collections import namedtuple
 
-Order = namedtuple('Order', 'id, items')
-Item = namedtuple('Item', 'type, description, amount, quantity')
+Order = namedtuple("Order", "id, items")
+Item = namedtuple("Item", "type, description, amount, quantity")
+
 
 def validorder(order: Order):
     net = 0
-    
+
     for item in order.items:
-        if item.type == 'payment':
-            net += item.amount
-        elif item.type == 'product':
-            net -= item.amount * item.quantity
+        if item.type == "payment":
+            if item.amount <= 1e6 and item.amount >= -1e6:
+                net += item.amount
+        elif item.type == "product":
+            if item.amount > 0 and item.amount * item.quantity <= 1e6 and item.amount >= 1:
+                net -= item.amount * item.quantity
         else:
-            return("Invalid item type: %s" % item.type)
-    
+            return "Invalid item type: %s" % item.type
+
     if net != 0:
-        return("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
+        return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
     else:
-        return("Order ID: %s - Full payment received!" % order.id)
+        return "Order ID: %s - Full payment received!" % order.id
