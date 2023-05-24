e1ce57589b0599259ac335d62a162a46a3e78f83
Collin Pfeifer <collinpfeifer@Icloud.com>
2023-05-24 01:58:28 +0000

    finished level 2

--- Level-2/code.h
+++ Level-2/code.h
@@ -44,6 +44,8 @@ bool update_setting(user_account* ua, const char *index, const char *value) {
     char *endptr;
     long i, v;
     i = strtol(index, &endptr, 10);
+    if(i < 0)
+        return false;
     if (*endptr)
         return false;
     if (i >= SETTINGS_COUNT)
